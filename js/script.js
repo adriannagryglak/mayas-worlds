25f14e7aedcd142fd3645672d2b47a23db3fee77
Adrianna Gryglak <adrianna.juda@live.com>
2021-03-15 15:50:12 +0100

    Start making worlds section

--- js/script.js
+++ js/script.js
@@ -1,3 +1,25 @@
+// ANIMATING TYPEING EFFECT 
+
+let index = 0;
+const typeingTitle = document.querySelector(".oval-background h1");
+const text = "Maya's Worlds";
+
+function typeing() {
+
+    let letter = text.slice(0, ++index);
+    typeingTitle.textContent = letter;
+
+    let timer = setTimeout(typeing, 500);
+    if (timer > 50) {
+        clearTimeout(timer);
+        
+    }
+};
+
+document.addEventListener("DOMContentLoaded", typeing());
+
+//ANIMATING WELCOME CAPTION
+
 const caption = document.querySelector('.welcome .welcome-caption');
 const welcomeSection = document.querySelector('.welcome');
 const mamRocket = document.querySelector('.mamrotek-rakieta-container');
@@ -7,8 +29,7 @@ const kopulyImg = document.querySelector('.kopuly-img');
 gsap.fromTo(caption, { y: '+=100', opacity: '0' }, {
     y: '0', opacity: '1', duration: 1, ease: 'easeInOut', scrollTrigger: {
         trigger: welcomeSection,
-        start: 'top 35%',
-    
+        start: 'top 35%'
     }
 });
 
@@ -26,38 +47,20 @@ animation.to(mamRocket, { x: window.innerWidth + mamRocket.offsetWidth, duration
     .to(mamRocket, { y: -welcomeSection.offsetHeight, duration: 4.5 }, 5.5)
     .to(mamRocket, { scale: 0.3, duration: 3.5 }, 6.5);
 
-gsap.to(kopulyImg, {y: -35, duration: 1, scrollTrigger: {
-    trigger: kopulyImg,
-    start: 'top 35%',
-    end: 'bottom 70%',
-    scrub: 1
-}});
+gsap.to(kopulyImg, {
+    y: -35, duration: 1, scrollTrigger: {
+        trigger: kopulyImg,
+        start: 'top 35%',
+        end: 'bottom 70%',
+        scrub: 1
+    }
+});
 
 /* TO DO's
 -odjezdza rakieta zle w mobile
 -odsuwanie img kopuł podczas scrolowania po sekcji sisters jest zależne od pikseli, przez co mobile się rozjeżdza
 -rozmiar czcionki w sekcji sisters w mobile jest za duży 
-
+-czemu po scrolowaniu ponizej home animacja typeingu sie zatrzymuje bez progresu ?
 */
 
-// ANIMATING TYPEING EFFECT 
 
-let index= 0;
-const typeingTitle = document.querySelector(".oval-background h1");
-const text = "Maya's Worlds";
-
-function typeing(){
-
-    
-
-    let letter = text.slice(0, ++index);
-    typeingTitle.textContent = letter;
-
-    let timer = setTimeout(typeing, 500);
-   
-    if (timer >33){
-        clearTimeout(timer);
-    }
-};
-
-document.addEventListener("DOMContentLoaded", typeing());
