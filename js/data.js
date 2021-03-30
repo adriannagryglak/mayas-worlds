f99aab8226f92f1450e6c42784fe01b07339cb9f
Adrianna Gryglak <adrianna.juda@live.com>
2021-03-30 22:09:55 +0200

    Styled test section in subapge, added images to it

--- js/data.js
+++ js/data.js
@@ -2,34 +2,101 @@ const dataSource = {}; // eslint-disable-line no-unused-vars
 
 dataSource.questions = {
     1: {
-        caption: 'lorem ipsum',
+        caption: 'Gdyby ten Kotek miał pseudonim, brzmiałby on...',
         options: {
-            a: 'expeliarmus',
-            b: 'expeliarmus',
-            c: 'expeliarmus',
+            a: ['Mamrobert Kocica'],
+            b: ['Mamrucent van Koth', 'true'],
+            c: ['Mamrozart'],
         },
-        image: '<img src="../images/mamrotek-sneak.png" alt="">',
+        image: '<img src="../images/kotki-mamrotki/mamrotek-painter.png" alt="">',
     },
 
     2: {
-        caption: 'lorem ipsum',
+        caption: 'Co wyprawia ten Mamrotek ?',
         options: {
-            a: '445',
-            b: '466',
-            c: '566l',
-            d: '23',
-            e: '56',
+            a: ['kręci piruety'],
+            b: ['puszcza bąki'],
+            c: ['ucieka przed tornadem'],
+            d: ['biegową życiówkę', 'true'],
         },
-        image: '<img src="../images/mamrotek-sneak.png" alt="">',
+        image: '<img src="../images/kotki-mamrotki/mamrotek-runner.png" alt="">',
     },
 
     3: {
-        caption: 'lorem ipsum',
+        caption: 'Co na myśli ma ten Kotek ?',
         options: {
-            a: 'LOL',
-            b: 'LOL',
-            c: 'LOLI'
+            a: ['przełóż, podrzuć, złap, przełóż, podrzuć, złap', 'true'],
+            b: ['cztery mandarynki mam nikomu ich nie dam'],
+            c: ['fajnie tak leżeć na śniegu, jakiś narciarz trzymał tu chyba kijki']
         },
-        image: '<img src="../images/mamrotek-sneak.png" alt="">',
+        image: '<img src="../images/kotki-mamrotki/mamrotek-clown.png" alt="">',
+    },
+    4: {
+        caption: 'Profesja tego Mamrosława to...',
+        options: {
+            a: 'barista',
+            b: 'nauczyciel',
+            c: 'doradca podatkowy',
+            d: 'kelner',
+        },
+        image: '<img src="../images/kotki-mamrotki/mamrotek-biuro.png" alt="">',
+    },
+    5: {
+        caption: 'Ten kot...',
+        options: {
+            a: 'coś wąchał',
+            b: 'gra na instrumencie muzycznym',
+            c: 'nie spał w nocy',
+            d: 'miał wypadek na a4 i stawia trójkąt ostrzegawczy',
+        },
+        image: '<img src="../images/kotki-mamrotki/mamrotek-musician.png" alt="">',
+    },
+    6: {
+        caption: 'Przyszłość tego Mamrotka...',
+        options: {
+            a: 'raczej jest świetlana',
+            b: 'napewno na bogato',
+            c: 'za kratami, w pasiaku',
+            d: 'można opisać zdaniem "Czy gdyby Harry wyrzucał śmieci, nie miałby blizny?"',
+        },
+        image: '<img src="../images/kotki-mamrotki/mamrotek-thief.png" alt="">',
+    },
+    7: {
+        caption: 'Ten mamraciarz:',
+        options: {
+            a: 'śni o szusowaniu bo zamknęli mu stoki przez padnemię',
+            b: 'wyrwał kable ze ściany',
+            c: 'ma dziwne buty',
+            d: 'nabił kiełbaski na patyki i wszedł w ognisko',
+        },
+        image: '<img src="../images/kotki-mamrotki/mamrotek-ski.png" alt="">',
+    },
+    8: {
+        caption: 'Temu kotkowi',
+        options: {
+            a: 'jest niedobrze',
+            b: 'trzeba pomóc w nauce innych literek',
+            c: 'zaraz naleśnik spadnie na głowę',
+        },
+        image: '<img src="../images/kotki-mamrotki/mamrotek-scream.png" alt="">',
+    },
+    9: {
+        caption: 'Co się stało temu Kotu?',
+        options: {
+            a: 'coś wybuchło mu na głowie',
+            b: 'trudny klient, vege, gluten free i narzeka',
+            c: 'nie dostał gwiazdki mamrelin za futro w zupie',
+        },
+        image: '<img src="../images/kotki-mamrotki/mamrotek-cook.png" alt="">',
+    },
+    10: {
+        caption: 'Na jakiej trasie lata ten kotkobus?',
+        options: {
+            a: 'między planetarnej (na Kessel poniżej 12 parseków!)',
+            b: 'Dworzec Główny- Arkady Capitol',
+            c: 'Jesionowa 33 - Paseux',
+            d: 'Urocza - Przeurocza',
+        },
+        image: '<img src="../images/mamrotek-rakieta.png" alt="">',
     },
 }
--- js/kotki-mamrotki-script.js
+++ js/kotki-mamrotki-script.js
@@ -1,6 +1,5 @@
 /* global Handlebars*/
 {
-
     'use strict'
 
     class Question {
@@ -9,24 +8,21 @@
             this.data = data;
 
             this.rendertoTest();
-            console.log('instacja klasy Question :', this)
+            this.checkIfCorrect();
         }
         rendertoTest() {
 
-            // Pobieramy HTML z szablonem
-            const template = document.getElementById("question-template").innerHTML;
-
-            // Kompilujemy szablon
-            const fillTemplateWith = Handlebars.compile(template);
-
-            // Wygenerowanie gotowego kodu z szablonu
-            let newHtml = fillTemplateWith(this.data);
-
-            // Wstawienie kodu na stronę
-            document.getElementById("test").innerHTML += newHtml;
-
+            const template = document.getElementById("question-template").innerHTML; //pobierz szablon
+            const fillTemplateWith = Handlebars.compile(template); //użyj go w funkci stworzonej
+            let newHtml = fillTemplateWith(this.data); //użyj funckji z danymi
+            document.getElementById("test").innerHTML += newHtml; // wstaw na naszą strone
 
             
+        }
+        checkIfCorrect(){
+
+
+         
         }
     }
 
@@ -36,16 +32,45 @@
             this.data = dataSource;
         },
         initTest: function () {
-            console.log('to moje data', this.data);
+            
             for (let question in this.data.questions) {
                 new Question(question, this.data.questions[question]); //dla kazdego pytania tworzy instancje przekazując dane 'nazwe' i to co po niej w data.js
+            }
+
+            //animate 
+            const testSection = document.querySelector('.mamrotki-test');
+            const img = document.querySelector('.img-hint');
+            const caption = document.querySelector('.p-hint');
+        
+            gsap.set(img, {y: '-550'});
+        
+            gsap.fromTo(img, {x: '-700'},{x: '0', ease: 'easeInOut', duration : 2, scrollTrigger: {
+                trigger: img,
+                start: 'top 45%',
+            }});
+            gsap.fromTo(caption, {x: '-700'},{x: '0', ease: 'easeInOut', duration : 2, scrollTrigger: {
+                trigger: img,
+                start: 'top 45%'
+            }});
+        },
+        checkTest: function () {
+
+            const options = document.querySelectorAll('.question li');
+            for (let option of options){
+                option.addEventListener('click', function(){
+
+                    //nadaj klase 'choosed'
+                    //sprawdz czy ma element true
+                    //jesli ma to co? 
+                    console.log('KLIK');
+                })
             }
         },
         init: function () {
             this.initData();
             this.initTest();
+            this.checkTest();
         }
-
     }
 
     app.init();
